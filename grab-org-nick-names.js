0fcb7cabf89e7398cbcd1e8bf8219b4b6ff3bf61
Robert Giesecke <robert.giesecke@gmail.com>
2019-08-05 22:17:59 +0200

    restore selection in grab-org-nick-names

--- grab-org-nick-names.js
+++ grab-org-nick-names.js
@@ -5,9 +5,34 @@
 
 		const searchBox = document.querySelector('input[name=search]');
 		searchBox.value = nickNamesAsJSon;
-		searchBox.focus();
-		searchBox.select();
-		document.execCommand('copy');
+
+		const getSelection = window.documentOrShadowRootInstance && documentOrShadowRootInstance.getSelection
+			? documentOrShadowRootInstance.getSelection
+			: (window.getSelection
+				? window.getSelection
+				: null);
+		const previousSelection = getSelection
+			? getSelection()
+			: null;
+		const previousSelectionRange = previousSelection && previousSelection.getRangeAt && previousSelection.rangeCount
+			? previousSelection.getRangeAt(0)
+			: null;
+
+		try {
+			searchBox.focus();
+			searchBox.select();
+			searchBox.scrollIntoView();
+			document.execCommand('copy');
+
+		} finally {
+			/* restore previous selection */
+			if (previousSelectionRange) {
+				const currentSelection = getSelection();
+				currentSelection.removeAllRanges();
+				currentSelection.addRange(previousSelectionRange);
+			}
+		}
+
 		return nickNamesAsJSon
 	} finally {
 		if (window.scContactsBookmarklets && typeof window.scContactsBookmarklets.executeAfter === 'function') {
