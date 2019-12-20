79472f381e863fea1884161cbc6a93754a4c9d92
Robert Giesecke <robert.giesecke@gmail.com>
2019-12-20 21:34:09 +0100

    use RSI.Api.* javascritp API instead of mimicking user input

--- follow-nick-names.js
+++ follow-nick-names.js
@@ -2,63 +2,69 @@
 /* it will exclude all existing friends and will try to find and follow every remaining nick name */
 (async () => {
 	try {
+		const makeRsiApiCallAsync = (rsiCall) => {
+			return (data) => new Promise((resolve) => rsiCall(resolve, data));
+		};
+
+		const getFriendNickNames = async () => {
+
+			var foundNickNames = [];
+
+			const listCall = makeRsiApiCallAsync(RSI.Api.Contacts.list);
+			var result = await listCall();
+
+			while (result && result.data) {
+				foundNickNames.push(...result.data.resultset.map(c => c.nickname));
+
+				if (!result.data.pagecount) {
+					result = null;
+					break;
+				}
+				const nextData = {
+					page: result.data.page + 1,
+					cursor: result.data.cursor
+				}
+				result = await listCall(nextData);
+			}
+			return foundNickNames;
+		}
+
+		const followNickName = (() => {
+			const addCall = makeRsiApiCallAsync(RSI.Api.Contacts.add);
+			return async (nickName) => {
+				const d = await addCall({ nickname: nickName });
+				// tell the user if it worked
+				if (d.success === 0) {
+					switch (d.code) {
+						case 'ErrNoAccountForNickname':
+						case 'ErrContactRelationNotFound':
+							console.info('user not found', nickName, 'message:', d.msg);
+							break;
+						default:
+							console.warn('Could not add nick', nickName, 'error:', d.msg);
+							console.debug(d);
+							break;
+					}
+				}
+				else {
+					console.log("added nick " + nickName);
+				}
+			}
+		})();
+
 		const searchBox = document.querySelector('.js-contacts-search-input');
+
 		const ownProfileHandleLink = document.querySelector('.c-account-sidebar__profile-info-handle');
 		const ownProfileHandle = ownProfileHandleLink ? ownProfileHandleLink.textContent : null;
-
-		const contactsDiv = document.querySelector('#sidebar-contacts-list');
-		const userNicks = JSON.parse(searchBox.value);
-		const searchSubmit = document.querySelector('.js-submit-contacts-search');
 		const clearSearchBox = () => {
 			if (contactsDiv.classList.contains('is-searching')) {
 				searchSubmit.click();
 			}
 		};
-		const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-		const getNickOfFollowLink = a => a.parentElement.parentElement.querySelector('.js-nickname');
-
-		/* scroll contacts to the end to get existing friends */
-		const getExistingNickNames = async (maxRetry, linkSelector) => {
-			const contactListElement = document.querySelector('.js-contacts-list');
-			const contactListQuery = jQuery(contactListElement);
-			await contactListQuery.stop().animate({
-				scrollTop: contactListQuery[0].scrollHeight
-			}, 2500).promise();
-
-			/* if there's unloaded nicks, retry scrolling until all are loaded */
-			var rescrollRetries = 0;
-			var unloadedNickCount = 0;
-			const getUnloadedNickCount = function () {
-				return unloadedNickCount = document.querySelectorAll('.isnt-loaded').length;
-			}
-			while (rescrollRetries++ < maxRetry && getUnloadedNickCount() > 0) {
-				console.log("found " + unloadedNickCount + "unloaded nick names, retry scolling...");
-				await contactListQuery.stop().animate({
-					scrollTop: 0
-				}, 500).promise();
-
-				await contactListQuery.stop().animate({
-					scrollTop: contactListQuery[0].scrollHeight
-				}, 1500).promise();
-			}
-
-			if (unloadedNickCount) {
-				console.log('there\'re still unloaded nick names, will continue with what I have...');
-			}
-
-			/* wait 0.5 secs for things to settle */
-			await sleep(500)
-
-			return Array.from(document.querySelectorAll('.js-nickname'))
-				.filter(t => {
-					/* only return nicks without visible (un)follow link */
-					const followLink = t.parentElement.querySelector(linkSelector);
-					return followLink && window.getComputedStyle(followLink).display != 'none';
-				})
-				.map(t => t.textContent);
-		}
-
-		const alreadyFollowingNickNames = await getExistingNickNames(3, '.js-unfollow');
+		const contactsDiv = document.querySelector('#sidebar-contacts-list');
+		const userNicks = JSON.parse(searchBox.value);
+		clearSearchBox();
+		const alreadyFollowingNickNames = await getFriendNickNames();
 
 		for (user of userNicks) {
 			if (user == ownProfileHandle) {
@@ -69,52 +75,7 @@
 				console.log('already following ' + user);
 				continue;
 			}
-			clearSearchBox();
-			searchBox.focus();
-			searchBox.value = user;
-			if (searchBox.value != user) {
-				clearSearchBox();
-				await sleep(200);
-				searchBox.value = user;
-			}
-			searchSubmit.click();
-
-			await sleep(400);
-
-			const followLinks = Array.from(contactsDiv.querySelectorAll('.js-follow'));
-			const followLinksWithNick = followLinks.filter(a => {
-				var nickLink = getNickOfFollowLink(a);
-				return nickLink && nickLink.textContent == user;
-			});
-			var shouldClearSearchbox = true;
-			try {
-				if (followLinksWithNick.length == 1) {
-					const followLink = followLinksWithNick[0];
-					const linkStyle = window.getComputedStyle(followLink);
-					if (linkStyle.display == 'none') {
-						console.log('already following ' + user);
-					} else {
-						followLinksWithNick[0].click();
-						console.log('clicked follow ' + user);
-					}
-				} else {
-					/* if you already follow the user, the result will be displayed twice, */
-					/* both with hidden follow link */
-					if (followLinksWithNick.length == 2) {
-						if (followLinksWithNick.filter(t => window.getComputedStyle(t).display == 'none').length) {
-							console.log('already following ' + user);
-							continue;
-						}
-					}
-					shouldClearSearchbox = false;
-					console.log('couldn\'t find ' + user);
-				}
-			}
-			finally {
-				if (shouldClearSearchbox) {
-					clearSearchBox();
-				}
-			}
+			await followNickName(user)
 		};
 	} finally {
 		if (window.scContactsBookmarklets && typeof window.scContactsBookmarklets.executeAfter === 'function') {
--- grab-org-nick-names.js
+++ grab-org-nick-names.js
@@ -1,9 +1,57 @@
-(function () {
+(async () => {
 	try {
 		const orgDiv = document.querySelector('#organization');
-		const nickNamesAsJSon = orgDiv ? JSON.stringify(Array.from(orgDiv.querySelectorAll('.nick')).map(t => t.textContent)) : null;
+		if (!orgDiv) {
+			return;
+		}
+		const orgSymbols = Array.from(orgDiv.querySelectorAll('.symbol'));
 
-		const searchBox = document.querySelector('input[name=search]');
+		if (!orgSymbols.length) {
+			return;
+		}
+		const orgName = orgSymbols[0].textContent;
+
+		const makeRsiApiCallAsync = (rsiCall) => {
+			return (data) => new Promise((resolve) => rsiCall(resolve, data));
+		};
+
+		const getAllOrgNickNames = (() => {
+			const getOrgMembersAsync = makeRsiApiCallAsync(RSI.Api.Org.getOrgMembers);
+
+			return async (orgName) => {
+				const nickNames = [];
+				var result = await getOrgMembersAsync({ symbol: orgName });
+				var currentPage = 1;
+				while (result && result.success && result.data && result.data.html) {
+					const detachedDiv = document.createElement('div');
+					detachedDiv.innerHTML = result.data.html;
+					const orgChunkNickNames = Array.from(detachedDiv.querySelectorAll('.nick')).
+						map(t => t.textContent).
+						filter(n => n && n.trim());
+					nickNames.push(...orgChunkNickNames);
+					currentPage += 1;
+
+					result = await getOrgMembersAsync({
+						symbol: orgName,
+						page: currentPage
+					});
+				}
+				return nickNames;
+			};
+		})();
+
+
+		const orgNickNames = await getAllOrgNickNames(orgName);
+		const nickNamesAsJSon = JSON.stringify(orgNickNames);
+
+
+		var searchBox = document.querySelector('input[name=search]');
+		const removeSearchBox = !searchBox;
+		if (removeSearchBox) {
+			searchBox = document.createElement('input');
+			document.body.appendChild(searchBox);
+		}
+
 
 		const getSelection = window.documentOrShadowRootInstance && documentOrShadowRootInstance.getSelection
 			? documentOrShadowRootInstance.getSelection
@@ -32,6 +80,9 @@
 				const currentSelection = getSelection();
 				currentSelection.removeAllRanges();
 				currentSelection.addRange(previousSelectionRange);
+			}
+			if (removeSearchBox) {
+				searchBox.remove();
 			}
 		}
 
